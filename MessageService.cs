474faeb0bb78317dd1591c04bce6a19ba9b04998
Szorad <david.szorad@accenture.com>
2017-08-22 11:21:27 +0200

    Created new Message Service

---
+++ MessageService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EventsApp
+{
+    public class MessageService
+    {
+        public void OnVideoEncoded(object source, EventArgs e)
+        {
+            Console.WriteLine("MessageService: Sending a text message...");
+        }
+    }
+}
