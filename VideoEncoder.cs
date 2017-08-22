eaffd16744913a4cc55a955d0885aba168e63cbb
Szorad <david.szorad@accenture.com>
2017-08-22 11:41:20 +0200

    Simplified by using EventHandler

--- VideoEncoder.cs
+++ VideoEncoder.cs
@@ -16,9 +16,7 @@ namespace EventsApp
         // 2- Define an event based on that delegate
         // 3- Raise the event
 
-        public delegate void VideoEncodedEventHandler(object source, VideoEventArgs args);
-
-        public event VideoEncodedEventHandler VideoEncoded;
+        public event EventHandler<VideoEventArgs> VideoEncoded;
 
         public void Encode(Video video)
         {
