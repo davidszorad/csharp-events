7bc1a20955e4b1cca4a4ddb8d00d2b51b76ec603
Szorad <david.szorad@accenture.com>
2017-08-22 11:12:59 +0200

    Created Mail Service

---
+++ MailService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EventsApp
+{
+    public class MailService
+    {
+        public void OnVideoEncoded(object source, EventArgs e)
+        {
+            Console.WriteLine("MailService: Sending an email...");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace EventsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var video = new Video() { Title = "Video 1" };
+            var videoEncoder = new VideoEncoder(); //publisher
+            var mailService = new MailService(); //subscriber
+
+            videoEncoder.VideoEncoded += mailService.OnVideoEncoded;
+            videoEncoder.Encode(video);
+        }
+    }
+}
